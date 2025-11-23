d507a942dd49fb9f52df7376ddf1f3efcb634866
Ivan_Gzhibovskiy <gzhibovskiy02@mail.ru>
2025-11-23 15:04:51 +0300

    Spring boot firs commit

---
+++ src/main/java/ohhudead/reservationsystem/dto/ProductRequest.java
@@ -0,0 +1,11 @@
+package ohhudead.reservationsystem.dto;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+
+public class ProductRequest {
+
+}
---
+++ src/main/java/ohhudead/reservationsystem/entity/Product.java
@@ -0,0 +1,38 @@
+package ohhudead.reservationsystem.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+
+@Entity
+@Table(name = "products")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+    @Column(name = "product_name", nullable = false)
+    private String name;
+
+    @Column(name = "price", nullable = false)
+    private BigDecimal price;
+
+    @Column(name = "in_stock", nullable = false)
+    private Integer inStock;
+
+    @Column(name = "created_at", insertable = false, updatable = false)
+    private OffsetDateTime createdAt;
+}
