bc25c766df5653472f30877ad4eefb5799df1a09
Ivan_Gzhibovskiy <gzhibovskiy02@mail.ru>
2025-12-04 23:32:47 +0300

    сделал GlobalExceptionHandler через builder, теперь он возвращает нужный HTTP статус

--- src/main/java/ohhudead/reservationsystem/exception/GlobalExceptionHandler.java
+++ src/main/java/ohhudead/reservationsystem/exception/GlobalExceptionHandler.java
@@ -8,7 +8,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.Instant;
@@ -21,74 +20,103 @@ public class GlobalExceptionHandler {
 
     //1
     @ExceptionHandler(ApplicationException.class)
-    public ErrorResponse handleApplicationException(
+    public ResponseEntity<ErrorResponse> handleApplicationException(
             ApplicationException ex,
             HttpServletRequest request
     ) {
         HttpStatus status = ex.getStatus();
 
-        return new ErrorResponse(
-               Instant.now(),
-                status.value(),
-                status.getReasonPhrase(),
-                ex.getMessage(),
-                request.getRequestURI()
-        );
+        ErrorResponse body = ErrorResponse.builder()
+                .timestamp(Instant.now())
+                .status(status.value())
+                .error(status.getReasonPhrase())
+                .message(ex.getMessage())
+                .path(request.getRequestURI())
+                .build();
+
+        log.warn("Application exception: {}",ex.getMessage());
+
+        return ResponseEntity.status(status).body(body);
+
     }
 
     //2
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ErrorResponse handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
-                                                      HttpServletRequest request) {
-        String message = ex.getBindingResult().getFieldErrors().stream()
+    public ResponseEntity<ErrorResponse> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex,
+            HttpServletRequest request)
+    {
+        String message = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
                 .map(error -> error.getField() + ":" + error.getDefaultMessage())
                 .collect(Collectors.joining(";"));
 
         HttpStatus status = HttpStatus.BAD_REQUEST;
 
-        return new ErrorResponse(
-                Instant.now(),
-                status.value(),
-                status.getReasonPhrase(),
-                ex.getMessage(),
-                request.getRequestURI()
-        );
+        ErrorResponse body =  ErrorResponse.builder()
+                .timestamp(Instant.now())
+                .status(status.value())
+                .error(status.getReasonPhrase())
+                .message(message)
+                .path(request.getRequestURI())
+                .build();
 
+        log.warn("validation failed: {}",message);
+
+        return ResponseEntity.status(status).body(body);
 
     }
+
     //3
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ErrorResponse handleConstraintViolation(
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<ErrorResponse> handleConstraintViolation(
             ConstraintViolationException ex,
             HttpServletRequest request
     ) {
 
-        String message = ex.getConstraintViolations().stream()
+        String message = ex.getConstraintViolations()
+                .stream()
                 .map(v -> v.getPropertyPath() + ":" + v.getMessage())
                 .collect(Collectors.joining(";"));
 
         HttpStatus status = HttpStatus.BAD_REQUEST;
 
-        return new ErrorResponse(
-                Instant.now(),
-                status.value(),
-                status.getReasonPhrase(),
-                message,
-                request.getRequestURI()
-        );
+        ErrorResponse body = ErrorResponse.builder()
+                .timestamp(Instant.now())
+                .status(status.value())
+                .error(status.getReasonPhrase())
+                .message(message)
+                .path(request.getRequestURI())
+                .build();
+
+        log.warn("Constraint violation: {}",message);
+
+        return ResponseEntity.status(status).body(body);
     }
+
     //4
     @ExceptionHandler(Exception.class)
-    public ErrorResponse handleOther(Exception ex, HttpServletRequest request) {
+    public ResponseEntity<ErrorResponse> handleOther(
+            Exception ex,
+            HttpServletRequest request
+    ) {
 
         HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
-        return new ErrorResponse(
-                Instant.now(),
-                status.value(),
-                status.getReasonPhrase(),
-                "Unexpected error",
-                request.getRequestURI()
-        );
+
+
+        ErrorResponse body = ErrorResponse.builder()
+                .timestamp(Instant.now())
+                .status(status.value())
+                .error(status.getReasonPhrase())
+                .message("Unexpected error")
+                .path(request.getRequestURI())
+                .build();
+
+        log.warn("Unexpected error", ex);
+
+        return ResponseEntity.status(status).body(body);
+
     }
 
 
